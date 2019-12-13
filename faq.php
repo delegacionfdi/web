e00df5cccf1d1d18d8fcf15e0259fdfbd9f0bc5c
Sawyer13 <dgarci13@ucm.es>
2019-12-13 19:28:58 +0100

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+	<title>Delegación de Alumnos de la Facultad de Informática de la Universidad Complutense de Madrid</title>
+		
+	<meta name="description" content="La mayor universidad presencial de España, con la mejor oferta educativa. En Madrid, rodeada de zonas verdes y con una intensa vida cultural y deportiva." />
+	<meta name="keywords" content="Universidad Complutense de Madrid, Facultad de Informática, FDI, DAFI, Delegación, Delegación de alumnos, Delegación de Estudiantes, Ingeniería Informática, Ingeniería de Software, Ingeniería Computadores, Software, Computadores, Desarrollo de Videojuegos, Videojuegos, Complutense, UCM, Educación, Innovación, Universidad de excelencia, Formación, Grado, Máster, Doctorado, Postgrado" />
+	<link rel="apple-touch-icon" href="img/logodafi.ico" />
+	<link rel="shortcut icon" href="img/logodafi.ico" />
+	<link rel="icon" href="img/logodafi.ico" />
+	<link type="text/css" media="screen" rel="stylesheet" href="assets/css/bootstrap.css" />
+	<link type="text/css" media="all" rel="stylesheet" href="assets/css/font-awesome.min.css" />
+	<link type="text/css" media="screen" rel="stylesheet" href="assets/css/ucm-theme.css" />
+	<link type="text/css" media="screen" rel="stylesheet" href="assets/css/ucm.css" />
+	<link type="text/css" media="screen" rel="stylesheet" href="assets/css/ucm-wg.css" />
+	<link type="text/css" media="print" rel="stylesheet" href="assets/css/print.css" />
+		
+	<!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WBQQDT6');</script>
+    <!-- End Google Tag Manager -->
+</head>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBQQDT6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+	<header>
+		<div id="barra">
+			<div class="container">
+								
+				<ul class="ul-menu">
+					<li>
+				        <div id="buscador">
+							<form action="https://google.com/search" method="get" class="posicion_buscar form-inline" id="formbuscador" target="_blank">
+								<input type="hidden" name="sitesearch" value="http://ucm.es" />
+								<label for="search" class="hidden">Buscar en la web</label>
+								<input type="text" id="search" name="q" placeholder="Buscar en la web" required />
+								<button type="submit" class="botonbusca" id="btsearch" title="Buscar en la web" aria-label="Buscar en la web"><span class="fa fa-search" aria-hidden="true" title="Buscar en la web"></span></button>
+							</form>
+								<button class="botoncerrar" id="cerrar_buscador" aria-label="Cerrar buscador"><span class="fa fa-times" aria-hidden="true" title="Cerrar buscador"></span></button>
+					    </div>
+					</li>
+					<li>	
+						<nav class="navbar" role="navigation" id="menusuperior">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-sup-collapse" id="collapse-personal">
+									<span class="sr-only">Desplegar navegación</span>
+									<span class="fa fa-user fa-2x"></span>
+					            </button>
+							</div>
+						</nav>				
+					</li>
+					<li>
+						<ul id="lg_items">
+							
+				<li><a href="https://www.informatica.ucm.es" title="Web FDI: Facultad de Informática" class="enlace">Web Facultad de Informática</a></li>						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="container" id="cabecera">
+			<div class="row">		
+				<div class="col-lg-6 col-sm-6 col-xs-12" id="logo">
+					<a href="http://delegacion.fdi.ucm.es/" title="Universidad Complutense de Madrid">
+						<img src="img/logo_dafi.png" alt="ucm" title="Universidad Complutense de Madrid">
+					</a>
+				</div>
+				<div class="col-lg-6 col-sm-6 col-xs-12" id="logo" style="text-align: right;">
+					<a href="https://www.ucm.es/" title="Universidad Complutense de Madrid">
+						<img src="img/logo.png" alt="ucm" title="Universidad Complutense de Madrid">
+					</a>
+				</div>
+				<div class="col-lg-7 col-sm-7 col-xs-12" id="titulo_website">
+					<h1 class="web_title_ucm"><a href="https://www.ucm.es/" title="Portada - Universidad Complutense de Madrid">Universidad Complutense de Madrid</a></h1>					
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<nav class="container navbar navbar-default" role="navigation" id="menu">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse" id="botonmenu">
+				<span class="sr-only">Desplegar navegación</span>
+				<span class="fa fa-bars"></span>
+			</button>
+		</div>
+ 
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			
+			<ul class="nav nav-justified" id="contenidomenu">
+				<li class="dropdown resalte lead">
+					<a title="Exámenes" href="https://drive.google.com/folderview?id=0B6xNlYS7GPDXbkFCblVDVXphMlk" target="_blank">Exámenes</a>
+				</li>
+				<li class="dropdown resalte lead">
+					<a title="Representación" href="/representacion" class="dropdown-toggle" data-toggle="dropdown">Representación</a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a title="RITSI" href="https://ritsi.org" target="_blank">RITSI</a></li>
+						<li><a title="Delegación Central de Estudiantes" href="https://www.ucm.es/dcestudiantes" target="_blank">Delegación Central de Estudiantes</a></li>
+						<li>
+							<a href="/estudiar" title="Estudiar" aria-label="Estudiar"><span class="fa fa-plus-circle" aria-hidden="true"  title="Estudiar"></span></a>
+						</li>
+					</ul>
+				</li>
+				<li class="dropdown resalte lead">
+					<a title="Recursos" href="/recursos" class="dropdown-toggle" data-toggle="dropdown">Recursos</a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a title="Investigación" href="https://drive.google.com/folderview?id=1jjO5sj_Y2rqnheXNkJoMti8JydjDx8O_" target="_blank">Actas de la ejecutiva</a></li>
+						<li><a title="OTRI" href="https://drive.google.com/folderview?id=1iPucKPT2JYEShUkJciyIGjukj3ttqrwm" target="_blank">Actas del pleno</a></li>
+						<li><a title="Servicio de Investigación" href="https://drive.google.com/a/ucm.es/file/d/1svKxtGAcvzxn8EMjasyOkM6s59U1sASS/view?usp=drivesdk" target="_blank">Estatutos</a></li>
+						<li><a href="/investigar" title="Investigar" aria-label="Investigar"><span class="fa fa-plus-circle" aria-hidden="true"  title="Investigar"></span></a></li>
+					</ul>
+				</li>
+				<li class="dropdown resalte lead">
+					<a title="Orla" href="orla.php">Orla</a>
+				</li>
+				<li class="dropdown resalte lead">
+					<a title="Impresora 3D" href="https://docs.google.com/a/ucm.es/forms/d/1KvZpQJbx_aJOTn2A--nbMh72vLrW6uAMsC1qKQTZrqQ/edit?usp=drivesdk" target="_blank">Impresora 3D</a>
+				</li>
+				<li class="dropdown resalte lead">
+					<a title="FAQ" href="faq.php">FAQ</a>
+				</li>
+				<li class="dropdown resalte lead">
+					<a title="Contacto" href="contacto.php">Contacto</a>
+				</li>
+			</ul>			
+		</div>
+	</nav>
+	
+				<div class="contact-content">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="contact-item">
+								<h2>Preguntas frecuentes</h2>
+								<ul>
+									<li>			
+										<div class="contact-info">
+											<h3>Lorem ipsum</h3>
+											<div class="contact-desc"><p></p></div>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+										</div>
+									</li>
+									<li>			
+										<div class="contact-info">
+											<h3>Lorem ipsum</h3>
+											<div class="contact-desc"><p></p></div>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+										</div>
+									</li>
+									<li>			
+										<div class="contact-info">
+											<h3>Lorem ipsum</h3>
+											<div class="contact-desc"><p></p></div>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+										</div>
+									</li>
+									<li>			
+										<div class="contact-info">
+											<h3>Lorem ipsum</h3>
+											<div class="contact-desc"><p></p></div>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> <!-- .contact-content -->
+	
+	<nav class="container text-center">
+		<ul class="redes">
+			<li><a id="link_yb" href="https://www.youtube.com/user/finformaticaucm" class="redsocial" target="_blank"><img alt="youtube" src="img/rrss/youtube.png" /></a></li>
+			<!--li><a id="link_fb" href="https://www.facebook.com/UniComplutense" class="redsocial" target="_blank"><img alt="facebook" src="img/rrss/facebook.png" /></a></li-->
+			<li><a id="link_tg" href="http://t.me/dafiucm" class="redsocial" target="_blank"><img alt="telegram" src="img/rrss/telegram.png" /></a></li>
+			<li><a id="link_tw" href="https://twitter.com/delegacionfdi" class="redsocial" target="_blank"><img alt="twitter" src="img/rrss/twitter.png" /></a></li>
+			<li><a id="link_in" href="https://www.instagram.com/delegacionfdi/" class="redsocial" target="_blank"><img alt="instagram" src="img/rrss/instagram.png" /></a></li>
+		</ul>
+	</nav>	
+	<footer id="pie">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 col-xs-12">
+					<nav id="pie_1" style="margin: 0 auto;">
+						<ul>
+						<li id="175"><a href="/empleo-ucm" title="Estatuto del estudiante">Estatuto del Estudiante</a></li>
+						<li id="75"><a href="https://www.ucm.es/la-casa-del-estudiante/estatuto-del-estudiante-ucm/" title="Buzón de sugerencias y quejas">Sugerencias y quejas</a></li>
+						<li id="1206"><a href="https://informatica.ucm.es/delegacion-y-asociaciones" title="Delegación y asociaciones">Delegación y asociaciones</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="col-sm-4 col-xs-12">
+					<nav id="pie_2">
+						<ul>
+						<li id="177"><a href="https://informatica.ucm.es/ofertas-de-empleo" title="Ofertas de empleo">Ofertas de empleo</a></li>
+						<li id="12"><a href="https://gipe.ucm.es/authestu.php" title="Plataforma de prácticas">Plataforma de prácticas</a></li>
+						<li id="1361"><a href="http://www.fdi.ucm.es/migs/" title="Museo">Museo</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="col-sm-4 col-xs-12 cei">
+					<a href="https://www.ucm.es/hrs4r" target="_blank" title="UCM - HR Excellence in Research"><img src="img/hr.jpg" alt="HR Excellence in Research" /></a>
+					<a href="http://www.campusmoncloa.es/" target="_blank" title="CAMPUS DE EXCELENCIA INTERNACIONAL"><img src="img/cei.jpg" alt="CAMPUS DE EXCELENCIA INTERNACIONAL" /></a>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row" id="pie_contacto">
+				<div class="col-sm-3 col-xs-12">&copy; <a href="https://garciabaameiro.com" target="_blank"> Daniel García Baameiro </a> based on website <a href="https://ucm.es" target="_blank"> Universidad Complutense Madrid</a></div>
+				<div class="col-sm-3 col-xs-12"><a href="https://informatica.ucm.es/contacto" title="Contacto">Localización y contacto</a></div>
+				<div class="col-sm-2 col-xs-12"><a href="https://www.ucm.es/aviso-legal" title="Aviso Legal">Aviso Legal</a></div>
+				<div class="col-sm-3 col-xs-12"><a href="https://www.ucm.es/datos-personales" title="Protección de datos">Protección de datos</a></div>
+				<div class="col-sm-1 col-xs-12"><a href="https://ucm.es" title="Universidad Complutense de Madrid">UCM</a></div>
+			</div>
+		</div>		
+	</footer>
+		<script type="text/javascript" src="assets/js/jquery.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+	<script type="text/javascript" src="assets/js/ucm.js"></script>
+	<script type="text/javascript">
+	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	    ga('create', 'UA-299350-23', 'ucm.es');
+	    ga('create', 'UA-299350-1', 'ucm.es', 'ucmHistorico');
+	    ga('send', 'pageview');
+	    ga('ucmHistorico.send', 'pageview');
+	    setTimeout("ga('send','event','NoBounce','page visit over 5 seconds')",5000);
+	</script>
+	
+</body>
+</html>
